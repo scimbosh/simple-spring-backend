0ebe7fd779677ee7306af4949a0af08da294c8e8
scimbosh <pavel.shvyrev@yandex.ru>
2023-09-14 01:54:41 +0500

    Add simple login page

---
+++ src/main/kotlin/com/scimbosh/simplespringbackend/configure/MvcConfig.kt
@@ -0,0 +1,20 @@
+package com.scimbosh.simplespringbackend.configure
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class MvcConfig: WebMvcConfigurer  {
+
+    //http://localhost:8080/home
+    //user = user , password will generate and output in console
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/home").setViewName("home")
+        registry.addViewController("/").setViewName("home")
+        registry.addViewController("/hello").setViewName("hello")
+        registry.addViewController("/login").setViewName("login")
+    }
+
+}
